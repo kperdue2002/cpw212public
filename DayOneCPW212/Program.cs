092c17cb71c6928d12a50f5d889ab3657dd8a027
kyran <kyranperdue@gmail.com>
2020-01-06 14:09:58 -0800

    removed unused method paramaters

--- DayOneCPW212/Program.cs
+++ DayOneCPW212/Program.cs
@@ -8,7 +8,7 @@ namespace DayOneCPW212
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("welcome to my humble console abode.");
 
